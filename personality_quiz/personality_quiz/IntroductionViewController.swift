f361d9e7c5a388d083e1a88a3b6555372ccae061
Mark <mark@iMac2.local>
2019-04-23 10:09:26 -0400

    finished personality quiz

---
+++ personality_quiz/personality_quiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  personality_quiz
+//
+//  Created by Mark on 4/15/19.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+/// this unraps the and restarts the quiz
+class IntroductionViewController: UIViewController {
+    @IBAction func unwindToQuizIntroduction(segue:
+        UIStoryboardSegue) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ personality_quiz/personality_quiz/ResultsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ResultsViewController.swift
+//  personality_quiz
+//
+//  Created by Mark on 4/17/19.
+//  Copyright © 2019 Mark. All rights reserved.
+//this is the result screen which shows the final result of the quiz
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    var responses: [Answer]!
+ 
+   
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+        // Do any additional setup after loading the view.
+    }
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers:[AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by: { (pair1, pair2) -> Bool in
+            return pair1.value > pair2.value})
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
